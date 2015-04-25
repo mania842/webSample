c71356f867bbcdc9a852cc647b3636190aced14b
Yong <dev.yong842@gmail.com>
2015-04-25 17:33:21 -0400

    Watch user location

--- ng/common/directives/myMap.js
+++ ng/common/directives/myMap.js
@@ -6,17 +6,8 @@
 	    var link = function($scope, element, attrs) {
 	        var map, infoWindow;
 	        var markers = [];
-	        
-	        if (navigator.geolocation) {
-	    		navigator.geolocation.getCurrentPosition(function(position){
-	    			$scope.$apply(function(){
-	    				console.log("position", position.coords.latitude);
-	    				console.log("position", position.coords.longitude);
-	    				$scope.position = position.coords;
-	    				initMap();
-	    			});
-	    	    });
-	    	}
+	        var watchId = undefined;
+	        var currentPositionMarker = undefined;
 	        
 	        // map config
 	        var mapOptions = {
@@ -26,28 +17,26 @@
 	            scrollwheel: true
 	        };
 	        
-	        
-	        
 	        // init the map
-	        function initMap() {
+	        var initMap = function() {
 	            if (map === void 0) {
-	            	if ($scope.setCurrent) {
-	    	        	mapOptions.center = new google.maps.LatLng($scope.position.latitude, $scope.position.longitude);
-	    	        }
+//	            	if ($scope.setCurrent) {
+//	    	        	mapOptions.center = new google.maps.LatLng($scope.position.latitude, $scope.position.longitude);
+//	    	        }
 	            	
 	                map = new google.maps.Map(element[0], mapOptions);
 	                setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
 	    	        setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
 	    	        setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
 	    	        
-	    	        if ($scope.setCurrent) {
-	    	        	setMarker(map, mapOptions.center, 'Yong', 'Text here', true);
-	    	        }
+//	    	        if ($scope.setCurrent) {
+//	    	        	setMarker(map, mapOptions.center, 'Yong', 'Text here', true);
+//	    	        }
 	            }
-	        }    
+	        };
 	        
 	        // place a marker
-	        function setMarker(map, position, title, content, isMyLocation) {
+	        var setMarker = function (map, position, title, content, isMyLocation) {
 	            var marker;
 	            var markerOptions = {
 	                position: position,
@@ -89,7 +78,56 @@
 		                infoWindow.open(map, marker);
 		            });
 	            }
-	        }
+	            
+	            return marker;
+	        };
+	        
+	      //Cancel watching
+	    	$scope.$on("$destroy", function(){
+	    		if (watchId != null)
+	    			navigator.geolocation.clearWatch(watchId);
+	        });
+	        
+	        initMap();
+	        
+	        if (navigator.geolocation) {
+//	    		navigator.geolocation.getCurrentPosition(function(position){
+//	    			$scope.$apply(function(){
+//	    				console.log("position", position.coords.latitude);
+//	    				console.log("position", position.coords.longitude);
+//	    				$scope.position = position.coords;
+//	    			});
+//	    	    });
+	    		
+	    		navigator.geolocation.getCurrentPosition(function(position){
+	    			if (currentPositionMarker) return;
+	    				// Log that this is the initial position.
+	    				console.log( "Initial current position Found",  position.coords.latitude + " " + position.coords.longitude);
+	    				// Set initial marker to the map using the position.
+	    				currentPositionMarker = setMarker(map, mapOptions.center, 'Yong', 'Text here', true);
+	    				console.log("currentPositionMarker.getPosition()", position);
+	    				
+	    				var center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	    			    // using global variable:
+	    			    map.panTo(center);
+	    			    map.setZoom(16);
+	    				
+	                    },
+	                    function( error ) {
+	                        console.log( "Something went wrong while initializing google map:", error );
+	                    },
+	                    {
+	                        timeout: Infinity,
+	                        maximumAge: 0,
+	                        enableHighAccuracy: true
+	                    }
+	                );
+	    		
+	    		watchId = navigator.geolocation.watchPosition( function( position ){
+                    // Set the new position of the existing marker.
+	    			currentPositionMarker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+	    		});
+	    	}
 	    };
 	    
 	    return {
