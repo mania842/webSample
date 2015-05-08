2a9c6a0552345212b2bd92b0d019c7aecb8c1add
Yong <dev.yong842@gmail.com>
2015-05-08 14:54:38 -0400

    dd

--- ng/common/module/module.js
+++ ng/common/module/module.js
@@ -37,7 +37,14 @@
 	
 	angular.module('myApp').controller('HomeController', 
     		function ($scope, $location, page) {
+		var setViewport = function(){
+			var meta = document.createElement('meta');
+			meta.name= "viewport";
+			meta.content = "width=device-width, initial-scale=1";
+			document.getElementsByTagName('head')[0].appendChild(meta);
+		};
 		
+		setViewport();
 //		console.log("$location.path()", $location.path());
 		$scope.page = page;
 	});
