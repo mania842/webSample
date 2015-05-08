d3b24456cdd65097cee15879ffc5561aa5eaabb6
Yong <dev.yong842@gmail.com>
2015-05-08 13:11:17 -0400

    updated buffet module

--- ng/buffet/module/module.js
+++ ng/buffet/module/module.js
@@ -11,9 +11,13 @@
 	var module = angular.module('buffetModule', [ 'ngRoute' ]);
 	// Configure app
 	module.config(function($routeProvider) {
-		$routeProvider.when('/buffet/menu/:homepage', {
+		$routeProvider.when('/buffet/:homepage', {
+			redirectTo : '/buffet/menu/:homepage'
+				
+		}).when('/buffet/menu/:homepage', {
 			templateUrl: 'ng/buffet/html/buffetMenu.html',
 			controller: 'BuffetMenuController'
+				
 		}).when('/buffet/price/:homepage', {
 			templateUrl: 'ng/buffet/html/buffetPrice.html',
 			controller: 'BuffetMenuController'
