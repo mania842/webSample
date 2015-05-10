9a5577d6809af03b9fee8bd2618c5f17c6966406
Yong <dev.yong842@gmail.com>
2015-05-10 09:20:14 -0400

    larger map

--- ng/common/controller/locationController.js
+++ ng/common/controller/locationController.js
@@ -5,6 +5,10 @@
     angular.module('myApp').controller('LocationController', 
     		function ($scope, $location) {
     		
+    	
+//    	https://www.google.com/maps/place/Gainesville+Home+Cooking/@34.296189,-83.83918,15z/data=!4m2!3m1!1s0x0:0xd7d439217d2283fb
+//    		
+//    	https://maps.google.com/maps?ie=UTF8&cid=15908427961649518868&q=Jangsujang+Restaurant&gl=US&hl=en&t=m&ll=33.958863,-84.138837&spn=0.008543,0.013733&z=16&iwloc=A&source=embed
     	console.log("location Controller");
     	$scope.data = {
     		phone: "917-504-9043",
